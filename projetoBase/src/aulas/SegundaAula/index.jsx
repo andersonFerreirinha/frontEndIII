c9833c749879ab26eff0887683902d42b18c3e41
ferreirinha <anderson_ferreirinha@hotmail.com>
2022-10-20 01:48:39 -0300

    adicionando arquivos da segunda aula

---
+++ projetoBase/src/aulas/SegundaAula/index.jsx
@@ -0,0 +1,62 @@
+import './style.scss'
+
+export function SegundaAula() {
+    return (
+        <main>
+            <h1>Segunda Aula</h1>
+            <h2>Projeto escolhido: globo</h2>
+            <a href="https://www.globo.com/">site globo</a>
+            <ul>
+                <li>
+                    <h1>Primeiro componente</h1>
+                    <p>Menu da pagina</p>
+                </li>
+                <li>
+                    <h1>Segundo componente</h1>
+                    <p>Itens de menu</p>
+                </li>
+                <li>
+                    <h1>Terceiro componente</h1>
+                    <p>Imagens das noticias</p>
+                </li>
+                <li>
+                    <h1>Quarto componente</h1>
+                    <p>Titulos das noticias</p>
+                </li>
+
+                <li>
+                    <h1>Quinto componente</h1>
+                    <p>Descrição das noticias</p>
+                </li>
+
+                <li>
+                    <h1>Sexto componente</h1>
+                    <p>Titulo das categorias das noticias, ex: esportes, economia, esportes, horóscopo</p>
+                </li>
+
+                <li>
+                    <h1>Sétimo componente</h1>
+                    <p>Botões dos signos na categoria de horóscopo</p>
+                </li>
+
+                
+                <li>
+                    <h1>Oitavo componente</h1>
+                    <p>Tabela de jogos na categoria esporte</p>
+                </li>
+
+                <li>
+                    <h1>Nono componente</h1>
+                    <p>Simbolo dos times nos placares da tabela de jogos</p>
+                </li>
+
+                <li>
+                    <h1>Décimo componente</h1>
+                    <p>Placar na tabela de jogos na categoria esporte</p>
+                </li>
+
+
+            </ul>
+        </main>
+    )
+}
